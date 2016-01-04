72cb051891e98aefafc7df1c37a9d3354e4edecb
Shavauhn Gabay <shavyg2@gmail.com>
2016-01-04 14:06:25 -0500

    saving

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,32 @@
 'use strict';
 var gulp = require("gulp")
-var watch = require("gulp-watch")
 var babel = require("gulp-babel")
 var print = require("gulp-print");
 var newer = require("gulp-newer")
+var shell = require("shelljs")
 
 
-gulp.task("watch", function() {
-  gulp.src("src/**/*.js").
-  pipe(watch("src/**/*.js")).
-  pipe(babel({
-    presets: ["es2015", "stage-0"],
-    plugins: ["transform-runtime"]
-  })).
-  pipe(print(function(file){
-    return "built "+file
-  })).
-  pipe(gulp.dest("dist"))
+gulp.task("documentation", function() {
+  shell.exec("npm run documentation");
 })
-gulp.task("build-all",["build"])
-gulp.task("build", function() {
-  let dest ="dist";
+
+gulp.task("build", ["documentation"], function() {
+  let dest = "dist";
   return gulp.src("src/**/*.js").
   pipe(newer(dest)).
   pipe(babel({
     presets: ["es2015", "stage-0"],
     plugins: ["transform-runtime"]
   })).
-  pipe(print(function(file){
-    return "built "+file
+  pipe(print(function(file) {
+    return "built " + file
   })).
   pipe(gulp.dest(dest))
+})
+
+gulp.task("build-all", ["build"])
+
+
+gulp.task("watch", ["build"], function() {
+  gulp.watch("src/**/*.js", ["build"])
 })
